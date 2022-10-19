f29deb41de95c7d4eec36de9504c733ce1a6a2e2
TangliziGit <tanglizimail@foxmail.com>
2022-10-19 17:48:20 +0800

    feat: complete typecast for insert values

---
+++ src/observer/sql/parser/typecast.h
@@ -0,0 +1,78 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include "rc.h"
+#include "parse_defs.h"
+
+RC chars_to(Value *value, AttrType type) {
+  switch (type) {
+    case CHARS:
+      break;
+    case INTS: {
+      int number = atoi((char *)value->data);
+      value_init_integer(value, number);
+      break;
+    }
+    case FLOATS: {
+      float number = atof((char *)value->data);
+      value_init_float(value, number);
+      break;
+    }
+    default:
+      return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+  return RC::SUCCESS;
+}
+
+RC ints_to(Value *value, AttrType type) {
+  switch (type) {
+    case INTS:
+      break;
+    case CHARS: {
+      char string[4];
+      sprintf(string, "%d", *(int *) value->data);
+      value_init_string(value, string);
+      break;
+    }
+    case FLOATS: {
+      value_init_float(value, *(int*)value->data);
+      break;
+    }
+    default:
+      return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+  return RC::SUCCESS;
+}
+
+RC floats_to(Value *value, AttrType type) {
+  switch (type) {
+    case FLOATS:
+      break;
+    case CHARS: {
+      char string[9];
+      sprintf(string, "%f", *(float *) value->data);
+      value_init_string(value, string);
+      break;
+    }
+    case INTS: {
+      value_init_integer(value, std::round(*(int *) value->data));
+      break;
+    }
+    default:
+      return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+  return RC::SUCCESS;
+}
+
+RC cast_to(Value *value, AttrType type) {
+  switch (value->type) {
+    case CHARS:
+      return chars_to(value, type);
+    case INTS:
+      return ints_to(value, type);
+    case FLOATS:
+      return floats_to(value, type);
+    default:
+      return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+}
